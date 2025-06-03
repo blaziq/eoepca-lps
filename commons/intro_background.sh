12a2651e9bdc53e649582e82bdd7eb37ff337627
Blazej Fitrzyk <blazej.fitrzyk@solenix.ch>
2025-06-03 21:34:09 +0200

    .

--- commons/intro_background.sh
+++ commons/intro_background.sh
@@ -52,15 +52,11 @@ if [[ -e /tmp/assets/minio.7z ]]; then
   # We have this locally installed for speed
   # wget -q https://dl.min.io/server/minio/release/linux-amd64/minio -O /usr/local/bin/minio && chmod +x /usr/local/bin/minio
   # wget -q https://dl.min.io/client/mc/release/linux-amd64/mc -O  /usr/local/bin/mc && chmod +x /usr/local/bin/mc
-  MINIO_ROOT_USER=eoepca
-  MINIO_ROOT_PASSWORD=eoepcatest
-  mkdir -p /usr/local/bin/
-  7z x /tmp/assets/minio.7z -o/usr/local/bin/
-  chmod +x /usr/local/bin/mc /usr/local/bin/minio
-  mkdir -p ~/minio 
-  nohup minio server --quiet ~/minio &> /dev/null &
+  mkdir -p /usr/local/bin/ && 7z x /tmp/assets/minio.7z -o/usr/local/bin/ && chmod +x /usr/local/bin/mc /usr/local/bin/minio
+  mkdir -p ~/minio && MINIO_ROOT_USER=eoepca MINIO_ROOT_PASSWORD=eoepcatest nohup minio server --quiet ~/minio &>/dev/null &
   sleep 1
   while ! mc config host add minio-local http://minio.eoepca.local:9000/ eoepca eoepcatest; do sleep 1; done
+  # mc alias set minio-local http://minio.eoepca.local:9000/ eoepca eoepcatest
   mc mb minio-local/eoepca
   mkdir -p ~/.eoepca && echo 'export S3_ENDPOINT="http://minio.eoepca.local:9000/"
 export S3_ACCESS_KEY="eoepca" 
