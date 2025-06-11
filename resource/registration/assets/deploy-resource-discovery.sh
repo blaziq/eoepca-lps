efa89c7cf3286c158784a9ca02ca6da24f957c48
Blazej Fitrzyk <blazej.fitrzyk@solenix.ch>
2025-06-11 02:48:48 +0200

    .

---
+++ resource/registration/assets/deploy-resource-discovery.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pushd ~/deployment-guide/scripts/resource-discovery 
+bash check-prerequisites.sh
+echo "nn" | bash configure-resource-discovery.sh
+helm repo add eoepca-dev https://eoepca.github.io/helm-charts-dev
+helm repo update eoepca-dev
+helm upgrade -i resource-discovery eoepca-dev/rm-resource-catalogue \
+  --values generated-values.yaml \
+  --version 2.0.0-rc1 \
+  --namespace resource-discovery \
+  --create-namespace
+kubectl --namespace resource-discovery wait pod --all --timeout=10m --for=condition=Ready
+kubectl apply -f generated-ingress.yaml
+sleep 5
+bash validation.sh
+popd
